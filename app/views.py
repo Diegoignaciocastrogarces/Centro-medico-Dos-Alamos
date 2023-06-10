ca01bde0a9a97c7f491b52008ab6eed48019e64b
DiegoIgnacioCastroGarces <diegoignaciocg0@gmail.com>
2023-06-10 16:18:10 -0400

    Interfaz de login lista

--- app/views.py
+++ app/views.py
@@ -1,8 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 bp = Blueprint('views', __name__)
 
 
-@bp.route('/')
-def index():
+@bp.route('/', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['user_name']
+        password = request.form['user_password']
+        return f'Nombre de usuario: {username}, Contraseña: {password}'
     return render_template('login.html')
